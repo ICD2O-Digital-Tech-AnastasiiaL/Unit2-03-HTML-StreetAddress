dc99dfb07d3da93caed621678ff0cca84dd2e176
Anastasiia Lebedenko <anastasiia.lebedenko@stu.ocsb.ca>
2025-03-25 14:19:27 +0000

    Created input website with js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Anastasiia Lebedenko All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Anastasiia Lebedenko
+// Created on: March 2025
 // This file contains the JS functions for index.html
+
+function enterClicked () {
+  // this function gets user's first name and age and displays it back to user
+
+  // get first name from text field
+  let firstName = document.getElementById("street-name").value
+	// get age from text field and cast it to integer
+  let userAge = parseInt(document.getElementById("number-entered").value)
+
+  // display name and age back to user
+  document.getElementById('user-info').innerHTML = "Your address name is" + firstName + " and your " + userAge + " is address number"
+}
